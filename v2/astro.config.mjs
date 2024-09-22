eb7944d42fdfd926f5d6b02ca0bca649885821aa
riverofrancisco <francisco.rivero124@gmail.com>
2024-09-22 16:57:19 +0200

    prerender const on components

--- v2/astro.config.mjs
+++ v2/astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-
-//https://astro.build/config
+// https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  adapter: vercel({ edgeMiddleware: true,}),
+  adapter: vercel({
+    edgeMiddleware: true,
+  }),
   output: "hybrid",
 });
