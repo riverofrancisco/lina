79862415f41420842228b372804e497310a1e4e5
riverofrancisco <francisco.rivero124@gmail.com>
2024-09-23 15:22:02 +0200

    jpg to webp

--- v2/src/components/Gallery/Carousel/Carousel.tsx
+++ v2/src/components/Gallery/Carousel/Carousel.tsx
@@ -1,10 +1,13 @@
-import car1 from "../../../../public/media/pictures/carousel/holaguido.jpg";
-import car2 from "../../../../public/media/pictures/carousel/corrientes.jpg";
-import car3 from "../../../../public/media/pictures/carousel/lacabeza.jpg";
+import car1 from "../../../../public/media/pictures/carousel/guitarra.webp";
+import car2 from "../../../../public/media/pictures/carousel/holaguido.webp";
+import car3 from "../../../../public/media/pictures/carousel/corrientes.webp";
+import car5 from "../../../../public/media/pictures/carousel/lacabeza.webp";
+import car4 from "../../../../public/media/pictures/carousel/toma1.webp";
+
 import { ImageSlider } from "./ImageSlider";
 
 export function HomeCarousel() {
-  const pictures = [car1.src, car2.src, car3.src];
+  const pictures = [car1.src, car2.src, car3.src, car4.src, car5.src];
 
   return <ImageSlider imageUrls={pictures} />;
 }
