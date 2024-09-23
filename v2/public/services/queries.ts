8a30ee45ec6af775c42d82f258971122a0e24bca
riverofrancisco <francisco.rivero124@gmail.com>
2024-09-23 17:30:34 +0200

    gallery pics ordered

--- v2/public/services/queries.ts
+++ v2/public/services/queries.ts
@@ -1,24 +1,24 @@
 import supabase from "./database";
 
 export const getEvents = async () => {
-  console.log('geting events')
+  console.log("geting events");
   let { data: events, error } = await supabase.from("events").select("*");
   if (error) {
     console.error("Error fetching pictures:", error);
     return null;
   }
-  console.log(events)
+  console.log(events);
   return events;
 };
 
 export const getGalleryPictures = async () => {
-  console.log('geting pictures')
+  console.log("geting pictures");
   let { data: pictures, error } = await supabase.from("pictures").select("*");
   if (error) {
     console.error("Error fetching pictures:", error);
     return null;
   }
   console.log(pictures);
+  pictures.sort((a: any, b: any) => a.id - b.id);
   return pictures;
 };
-  
