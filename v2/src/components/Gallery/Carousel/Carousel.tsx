cfa082bdb38f4f03615ed6daf9588afef062066e
riverofrancisco <francisco.rivero124@gmail.com>
2024-09-25 23:57:09 +0200

    Carousel Pics3

--- v2/src/components/Gallery/Carousel/Carousel.tsx
+++ v2/src/components/Gallery/Carousel/Carousel.tsx
@@ -1,6 +1,6 @@
 import car1 from "../../../../public/media/pictures/carousel/carousel_1b.webp";
 import car2 from "../../../../public/media/pictures/carousel/corrientes.webp";
-import car3 from "../../../../public/media/pictures/carousel/guitarra.webp";
+import car3 from "../../../../public/media/pictures/carousel/carousel_1.webp";
 import car4 from "../../../../public/media/pictures/carousel/toma1.webp";
 import car5 from "../../../../public/media/pictures/carousel/carousel_5.webp";
 
