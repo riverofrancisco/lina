ef636695104034f6195556865bb0c712ef992de6
riverofrancisco <francisco.rivero124@gmail.com>
2025-04-19 13:40:16 +0200

    Update logo in production

---
+++ soylina/src/app/home/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is a simple test page for the Home route.</p>
+    </div>
+  );
+}
